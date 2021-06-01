81ce5182c4831dcbec97ba0809bda3d82055ec15
Elisama-QSL <elisamaqs@gmail.com>
2021-06-01 10:43:48 -0400

    primeiro commit

---
+++ Northwind/Northwind/Models/Cliente.cs
@@ -0,0 +1,9 @@
+namespace Northwind.Models
+{
+    public class Cliente
+    {
+     
+        public long Id{ get; set; }
+        public string  company_name { get; set; }
+    }
+}
---
+++ Northwind/Northwind/Models/ClienteContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Models
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
